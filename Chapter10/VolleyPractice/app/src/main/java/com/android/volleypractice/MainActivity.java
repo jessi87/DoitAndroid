baafaffd6e72d05aba24dd0735b6e9017eda96aa
jessi87 <jessiseo87@gmail.com>
2020-09-14 22:42:05 +0900

    initial

---
+++ Chapter10/VolleyPractice/app/src/main/java/com/android/volleypractice/MainActivity.java
@@ -0,0 +1,76 @@
+package com.android.volleypractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    TextView textView;
+
+    static RequestQueue queue; //요청 큐는 한번만 만들어서 계속 사용
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                makeRequest();
+            }
+        });
+
+        if (queue == null) {
+            queue = Volley.newRequestQueue(getApplicationContext()); //RequestQueue 객체 생성
+        }
+    }
+
+    private void makeRequest() {
+        String url = editText.getText().toString();
+        //요청을 보내기 위한 StringRequest 객체 생성(요청방식,사이트주소,응답받을 리스너객체,에러시 호출될 리스너객체)
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                println("응답 -> " + response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                println("에러 -> " + error.getMessage());
+            }
+        }) {
+            protected Map<String, String> getParams() throws AuthFailureError{
+                Map<String,String> params = new HashMap<String,String>();
+                return params;
+            }
+        };
+        request.setShouldCache(false);//이전 캐시를 사용하지 않겠다
+        queue.add(request); //요청 객체를 요청 큐에 넣음->요청 큐가 자동으로 요청, 응답과정 진행
+        println("요청 보냄");
+    }
+
+    void println(String data){
+        textView.append(data+"\n");
+    }
+}
