fe82c0fc42480c5115cd838e2bf19c696d5ceb30
jessi87 <jessiseo87@gmail.com>
2020-09-16 22:28:04 +0900

    initial

---
+++ Chapter10/GsonPractice/app/src/main/java/com/android/gsonpractice/MovieListResult.java
@@ -0,0 +1,10 @@
+package com.android.gsonpractice;
+
+import java.util.ArrayList;
+
+public class MovieListResult {
+    String boxofficeType;
+    String showRange;
+
+    ArrayList<com.android.gsonpractice.Movie> dailyBoxOfficeList = new ArrayList<>();
+}
